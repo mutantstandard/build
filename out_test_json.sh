65bad5e5da4e407f25ec3dd8014a1b018e602cca
Dzuk <32108484+dzuk-mutant@users.noreply.github.com>
2023-06-12 13:50:58 +0100

    Pruning unwanted build scripts

--- out_test_json.sh
+++
@@ -1,2 +0,0 @@
-cd "$(dirname "$0")"
-./orxporter/orxport.py -m manifest/out.orx -j out/test_json/mtnt_test.json
--- out_test_svg.sh
+++
@@ -1,3 +0,0 @@
-cd "$(dirname "$0")"
-./orxporter/orxport.py -m manifest/out.orx -i ../input -C cache -q 32x32 -o out/test_svg/codepoint -f %u  &&
-./orxporter/orxport.py -m manifest/out.orx -i ../input -C cache -q 32x32 -o out/test_svg/shortcode -f %d/%s
