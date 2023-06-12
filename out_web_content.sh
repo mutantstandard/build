e1651d9801cfb398afecf6eb30c45ccda68133b6
Dzuk <32108484+dzuk-mutant@users.noreply.github.com>
2023-06-12 14:02:53 +0100

    Removing more old and unwanted build scripts

--- out_web_content.sh
+++
@@ -1,3 +0,0 @@
-cd "$(dirname "$0")"
-./orxporter/orxport.py -m manifest/out.orx -i ../input -C cache -q 32x32 -o out/web_content/mutstd -f %s &&
-./orxporter/orxport.py -m manifest/out.orx -J out/web_content/emoji.json
--- out_web_content_json.sh
+++
@@ -1,2 +0,0 @@
-cd "$(dirname "$0")"
-./orxporter/orxport.py -m manifest/out.orx -J out/web_content/emoji.json
