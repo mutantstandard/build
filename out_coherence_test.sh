eafb744eeb959c213b3ac9bf810e4e815307f65a
Caius Nocturne <32108484+dzuk-mutant@users.noreply.github.com>
2023-12-21 14:38:27 +0000

    Smiley fixes
    For particular smilies, the 'Annoying 5' as I like to calm them, have all had their shortcodes changed to match Discord, GitHub, etc. because they're weird, vague and I don't have a better idea for what to call them.
    
    - smile -> slight_smile
    - big_smile -> grinning
    - happy -> smiley
    - grin -> smile
    - grinning -> grin
    - wink_kiss -> blowing_a_kiss
    
    - Also this slipped through the cracks many years ago - The idea for mild_panic was from cassidoo on Twitter and I assumed I had credited them as the originator of the idea, but I didn't and completely forgot, they have been added to the design credits now.
    
    - Coherence test sh now outputs to its own subfolder.

--- out_coherence_test.sh
+++ out_coherence_test.sh
@@ -1,2 +1,2 @@
 cd "$(dirname "$0")"
-./orxporter/orxport.py -m manifest/out.orx -i ../input -q 32x32 -o out/all_slice -r resvg -f %f/%s -t 8 -F svg
+./orxporter/orxport.py -m manifest/out.orx -i ../input -q 32x32 -o out/coherence -r resvg -f %f/%s -t 8 -F svg
