96ffff7fabc25192b2c45c167d0a75e45c55eb30
Caius Nocturne <32108484+dzuk-mutant@users.noreply.github.com>
2023-12-24 21:05:56 +0000

    Version bump

--- out_final_full.sh
+++ out_final_full.sh
@@ -3,7 +3,7 @@
 
 cd "$(dirname "$0")"
 
-version=2023.11
+version=2023.12
 render_threads=12
 base_dir="out/final"
 
--- out_final_slice.sh
+++ out_final_slice.sh
@@ -1,6 +1,6 @@
 cd "$(dirname "$0")"
 
-version=2023.11
+version=2023.12
 render_threads=12
 base_dir="out/test-pkg"
 
